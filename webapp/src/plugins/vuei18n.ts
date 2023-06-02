dcca756ee69daec449a3e406483994db61096b2e
ZACHARY KLOSKO <zklosko@hopecentral.com>
2023-06-02 12:35:58 -0400

    feat: vite handles vue-i18n bundling

--- webapp/src/plugins/vuei18n.ts
+++ webapp/src/plugins/vuei18n.ts
@@ -1,57 +1,13 @@
 import { createI18n } from 'vue-i18n';
+import messages from '@intlify/unplugin-vue-i18n/messages';
 
-// import csCZ from '../locale/cs_CZ.json';
-// import deAT from '../locale/de_AT.json';
-// import deDE from '../locale/de_DE.json';
-// import elGR from '../locale/el_GR.json';
-// import enCA from '../locale/en_CA.json';
-// import enGB from '../locale/en_GB.json';
-import enUS from '../locale/en_US.json';
-// import esES from '../locale/es_ES.json';
-// import frFR from '../locale/fr_FR.json';
-// import hrHR from '../locale/hr_HR.json';
-// import huHU from '../locale/hu_HU.json';
-// import itIT from '../locale/it_IT.json';
-// import jaJP from '../locale/ja_JP.json';
-// import koKR from '../locale/ko_KR.json';
-// import nlNL from '../locale/nl_NL.json';
-// import plPL from '../locale/nl_NL.json';
-// import ptBR from '../locale/pt_BR.json';
-// import ruRU from '../locale/ru_RU.json';
-// import srRS from '../locale/sr_RS.json';
-// import trTR from '../locale/tr_TR.json';
-// import ukUA from '../locale/uk_UA.json';
-// import zhCN from '../locale/zh_CN.json';
+export const allLocales: string[] = ['en_GB, en_US'];
 
-const i18n = createI18n({
+export const i18n = createI18n({
   legacy: false,
-  locale: 'en_US',
-  fallbackLocale: 'en_US',
   globalInjection: true,
-  messages: {
-    // csCZ,
-    // deAT,
-    // deDE,
-    // elGR,
-    // enCA,
-    // enGB,
-    enUS,
-    // esES,
-    // frFR,
-    // hrHR,
-    // huHU,
-    // itIT,
-    // jaJP,
-    // koKR,
-    // nlNL,
-    // plPL,
-    // ptBR,
-    // ruRU,
-    // srRS,
-    // trTR,
-    // ukUA,
-    // zhCN,
-  },
-});
-
+  locale: 'en_us',
+  fallbackLocale: 'en_us',
+  messages: messages
+})
 export default i18n;
